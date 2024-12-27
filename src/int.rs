f98c79d9e6b19e06f27ff39787f139d399550c48
Nikita Tikhonov <nekitdevofficial@gmail.com>
2024-12-27 21:50:19 +0300

    Fix `int::ParseError` diagnostic code.

--- src/int.rs
+++ src/int.rs
@@ -8,5 +8,5 @@ use thiserror::Error;
 /// Wraps [`ParseIntError`] to provide diagnostics.
 #[derive(Debug, Error, Diagnostic)]
 #[error("failed to parse integer")]
-#[diagnostic(code(totp::int::parse), help("ensure the input is valid"))]
+#[diagnostic(code(otp_std::int::parse), help("ensure the input is valid"))]
 pub struct ParseError(#[from] pub ParseIntError);
