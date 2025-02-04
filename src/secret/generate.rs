a269b2218c1798f9617c4f6cd726f38db835df94
Nikita Tikhonov <nekitdevofficial@gmail.com>
2025-02-04 22:01:24 +0300

    Bump dependencies, update `rand` usage accordingly.

--- src/secret/generate.rs
+++ src/secret/generate.rs
@@ -1,16 +1,14 @@
 //! Secret generation.
 
-use rand::{thread_rng, RngCore};
+use rand::{rng, RngCore};
 
 use crate::secret::length::Length;
 
 /// Generates cryprographically secure random bytes of specified length.
 pub fn generate(length: Length) -> Vec<u8> {
-    let mut rng = thread_rng();
-
     let mut secret = vec![0; length.get()];
 
-    rng.fill_bytes(&mut secret);
+    rng().fill_bytes(&mut secret);
 
     secret
 }
