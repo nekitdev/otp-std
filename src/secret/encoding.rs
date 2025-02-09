f9d998deafa63e5982bedddd94b221720aea0af4
Nikita Tikhonov <nekitdevofficial@gmail.com>
2025-02-09 20:04:22 +0300

    Make error handling in `secret::encoding` consistent.

--- src/secret/encoding.rs
+++ src/secret/encoding.rs
@@ -4,6 +4,8 @@ use base32::Alphabet;
 use miette::Diagnostic;
 use thiserror::Error;
 
+use crate::macros::errors;
+
 /// Represents errors that can occur when secret decoding fails.
 #[derive(Debug, Error, Diagnostic)]
 #[error("failed to decode `{secret}` secret")]
@@ -28,10 +30,10 @@ pub fn encode<S: AsRef<[u8]>>(secret: S) -> String {
     base32::encode(ALPHABET, secret.as_ref())
 }
 
-macro_rules! decode_error {
-    ($secret: expr) => {
-        Error::new($secret.to_owned())
-    };
+errors! {
+    Type = Error,
+    Hack = $,
+    error => new(secret => to_owned),
 }
 
 /// Decodes the given secret.
@@ -41,7 +43,7 @@ macro_rules! decode_error {
 /// Returns [`struct@Error`] if the secret could not be decoded.
 pub fn decode<S: AsRef<str>>(secret: S) -> Result<Vec<u8>, Error> {
     fn decode_inner(secret: &str) -> Result<Vec<u8>, Error> {
-        base32::decode(ALPHABET, secret).ok_or_else(|| decode_error!(secret))
+        base32::decode(ALPHABET, secret).ok_or_else(|| error!(secret))
     }
 
     decode_inner(secret.as_ref())
